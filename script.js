465695f692c59123525d80430b707c29f326225e
oscar <org734664@gmail.com>
2022-05-30 16:30:10 -0500

    Primeros Avances sobre mi proyecto

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeColor(event) {
+    document.getElementById("main").style.backgroundColor = event. value;
+    document.getElementById("pik").style.backgroundColor = event. value;
+    
+}
